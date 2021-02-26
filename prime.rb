c827322576971f77b12772b56bfc658623c7f371
speraza545 <s.peraza545@gmail.com>
2021-02-25 22:25:16 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+
+
+def prime?(n)
+    if n <= 1
+        return false
+      else 
+          if (2...n-1).any? { |i| n % i  == 0}
+              return false
+          else
+              return true
+          end 
+      end
+  end
